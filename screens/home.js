7427ec6f243443b7789e5761ed533c0bdbf4ad9a
fadzreen43 <fadzreen34@gmail.com>
2020-03-04 00:38:25 +0800

    formik form -> Input Form

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity, Modal} from 'react-native';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import {globalStyles} from '../styles/global';
 import Card from '../shared/card';
 import { MaterialIcons } from '@expo/vector-icons';
+import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }){
     const [modalOpen, setModalOpen] = useState(false);
@@ -11,20 +12,30 @@ export default function Home({ navigation }){
         { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
         { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
       ]);
+
+      const addReview = (review) =>{
+          review.key = Math.random().toString();
+          setReviews((currentReviews) => {
+                return[review, ...currentReviews];
+          });
+          setModalOpen(false);
+      }
     
     return(
         <View style={globalStyles.container}> 
 
             <Modal visible={modalOpen} animationType='slide'>
-                <View style={StyleSheet.modalContent}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.modalContent}>
                     <MaterialIcons 
                     name='close'
                     size={24}
                     style={{...styles.modalToggle, ...styles.modalClose}}
                     onPress={() => setModalOpen(false)}
                     />
-                    <Text>Hello From modal :)</Text>
+                    <ReviewForm addReview={addReview} />
                 </View>
+                </TouchableWithoutFeedback>
             </Modal>
 
            <MaterialIcons 
